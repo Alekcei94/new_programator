b029fd65be6a553d7d9a62a179b2c8bee65e8d7e
Alexey <Alekcei940421@gmail.com>
2022-03-01 15:05:21 +0300

    Небольште изменения при переносе программы

--- program/start_programs_script.py
+++ program/start_programs_script.py
@@ -241,16 +241,18 @@ class Commands_Window_OneWire_New_Analog(QtWidgets.QMainWindow):
         print("Старт измерений")
         logger.write_log("Старт измерений", 0)
         # TODO вставить логирование температур
-        list_temp_spec = [-60, -30, 0, 25, 50, 75, 100, 125]
-        other_devices.work_spec(-60)
-        for i in range(6):
-            print(f'Осталось {60 - i*10} минут, температура {list_temp_spec[0]}')
-            time.sleep(600)
-        helper_methods.read_temp_and_write_in_file(saveOption)
-        for temp_spec in list_temp_spec:
+        list_temp_spec = [-60, 0, 25, 50, 75, 100, 125]
+        # other_devices.work_spec(-60)
+        # for i in range(6):
+        #     print(f'Осталось {60 - i*10} минут, температура {list_temp_spec[0]}')
+        #     time.sleep(600)
+        # helper_methods.read_temp_and_write_in_file(saveOption)
+        for iterator in range(1, len(list_temp_spec)):
+            temp_spec = list_temp_spec[iterator]
             other_devices.work_spec(temp_spec)
-            for i in range(3):
-                print(f'Осталось {30 - i * 10} минут, температура {temp_spec}')
+            helper_methods.sleep_in_time(saveOption, temp_spec)
+            for i in range(2):
+                print(f'Осталось {20 - i * 10} минут, температура {temp_spec}')
                 time.sleep(600)
             helper_methods.read_temp_and_write_in_file(saveOption)
         print("Конец чтения температурного кода")
